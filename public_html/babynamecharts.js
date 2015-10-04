431ebd295cd9836a918b10882c61e36268b151e5
Josh Watzman <jwatzman@jwatzman.org>
2015-10-03 22:48:56 -0700

    Try highcharts after all

--- public_html/babynamecharts.js
+++ public_html/babynamecharts.js
@@ -7,15 +7,32 @@ xhr.onload = function (e) {
 	var db = new SQL.Database(uInt8Array);
 	window.db = db;
 
-	google.load('visualization', '1', {'packages':['corechart'], 'callback': function () {
-		var v = db.exec('SELECT year,rank FROM names WHERE name = "Joshua" AND gender = 1 ORDER BY year ASC;')[0].values;
-		var data = new google.visualization.DataTable();
-		data.addColumn('number', 'Year');
-		data.addColumn('number', 'Rank');
-		data.addRows(v);
-		var options = {'title': 'Boys named Joshua', 'vAxis': {direction: -1}, 'hAxis': {format: ''}};
-		var chart = new google.visualization.LineChart(document.getElementById('gchart'));
-		chart.draw(data, options);
-	}});
+	var v = db.exec('SELECT year,rank FROM names WHERE name = "Joshua" AND gender = 1 ORDER BY year ASC;')[0].values;
+
+	var chart = new Highcharts.Chart({
+		chart: {
+			type: 'line',
+			renderTo: 'chart',
+			zoomType: 'x'
+		},
+		series: [{
+			name: 'Rank',
+			data: v
+		}],
+		title: {
+			text: 'Boys named Joshua',
+		},
+		xAxis: {
+			minTickInterval: 1
+		},
+		yAxis: {
+			reversed: true,
+			min: 0,
+			minTickInterval: 1,
+			title: {
+				text: 'Rank'
+			}
+		}
+	});
 };
 xhr.send();
